7b6a1f433b22e5309b07f24b6679583628b9bea8
noe222 <nlucenae@alumnes.santjosepobrer.es>
2026-03-12 18:48:05 +0100

    problems fixed

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 // Fíjate en las comillas simples al principio y al final:
-const supabaseUrl='https://tqdzqynijzhcurzihsl.supabase.co';
+const supabaseUrl='https://tqdzqynijzhcurzihhsl.supabase.co';
 const supabaseAnonKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZHpxeW5panpoY3VyemloaHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNTY5NTQsImV4cCI6MjA4ODczMjk1NH0.WzLU8krpjJGpKRqroGnb7nxS-A5KQTvddT9ByPz3zuI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
